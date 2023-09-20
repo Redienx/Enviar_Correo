994f454f86ea95e853944b10e823d2a7b903ee07
Redienx <snrideriglesias5@gmail.com>
2023-09-20 18:41:34 -0500

    EnvioCorreo

---
+++ EnvioCorreo/EnvioCorreo.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace EnvioCorreo
+{
+    partial class fmrEnvioCorreo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.lblAsunto = new System.Windows.Forms.Label();
+            this.lblEnviarPara = new System.Windows.Forms.Label();
+            this.lblArchivos = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnArchivos = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtMensaje = new System.Windows.Forms.RichTextBox();
+            this.txtAsunto = new System.Windows.Forms.TextBox();
+            this.txtEmailPara = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(24, 129);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(62, 16);
+            this.lblMensaje.TabIndex = 19;
+            this.lblMensaje.Text = "Mensaje:";
+            // 
+            // lblAsunto
+            // 
+            this.lblAsunto.AutoSize = true;
+            this.lblAsunto.Location = new System.Drawing.Point(24, 76);
+            this.lblAsunto.Name = "lblAsunto";
+            this.lblAsunto.Size = new System.Drawing.Size(51, 16);
+            this.lblAsunto.TabIndex = 18;
+            this.lblAsunto.Text = "Asunto:";
+            // 
+            // lblEnviarPara
+            // 
+            this.lblEnviarPara.AutoSize = true;
+            this.lblEnviarPara.Location = new System.Drawing.Point(24, 26);
+            this.lblEnviarPara.Name = "lblEnviarPara";
+            this.lblEnviarPara.Size = new System.Drawing.Size(59, 16);
+            this.lblEnviarPara.TabIndex = 17;
+            this.lblEnviarPara.Text = "Enviar a:";
+            // 
+            // lblArchivos
+            // 
+            this.lblArchivos.AutoSize = true;
+            this.lblArchivos.Location = new System.Drawing.Point(122, 344);
+            this.lblArchivos.Name = "lblArchivos";
+            this.lblArchivos.Size = new System.Drawing.Size(16, 16);
+            this.lblArchivos.TabIndex = 16;
+            this.lblArchivos.Text = "...";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(122, 317);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(116, 16);
+            this.lblTitulo.TabIndex = 15;
+            this.lblTitulo.Text = "Archivos adjuntos:";
+            // 
+            // btnArchivos
+            // 
+            this.btnArchivos.Location = new System.Drawing.Point(125, 458);
+            this.btnArchivos.Name = "btnArchivos";
+            this.btnArchivos.Size = new System.Drawing.Size(164, 25);
+            this.btnArchivos.TabIndex = 14;
+            this.btnArchivos.Text = "Adjuntar archivos";
+            this.btnArchivos.UseVisualStyleBackColor = true;
+            this.btnArchivos.Click += new System.EventHandler(this.btnFiles_Click);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(443, 458);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 13;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(125, 126);
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(393, 177);
+            this.txtMensaje.TabIndex = 12;
+            this.txtMensaje.Text = "";
+            // 
+            // txtAsunto
+            // 
+            this.txtAsunto.Location = new System.Drawing.Point(125, 76);
+            this.txtAsunto.Name = "txtAsunto";
+            this.txtAsunto.Size = new System.Drawing.Size(393, 22);
+            this.txtAsunto.TabIndex = 11;
+            // 
+            // txtEmailPara
+            // 
+            this.txtEmailPara.Location = new System.Drawing.Point(125, 26);
+            this.txtEmailPara.Name = "txtEmailPara";
+            this.txtEmailPara.Size = new System.Drawing.Size(393, 22);
+            this.txtEmailPara.TabIndex = 10;
+            // 
+            // fmrEnvioCorreo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 509);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.lblAsunto);
+            this.Controls.Add(this.lblEnviarPara);
+            this.Controls.Add(this.lblArchivos);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnArchivos);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.txtAsunto);
+            this.Controls.Add(this.txtEmailPara);
+            this.Name = "fmrEnvioCorreo";
+            this.Text = "Envio de Correo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Label lblAsunto;
+        private System.Windows.Forms.Label lblEnviarPara;
+        private System.Windows.Forms.Label lblArchivos;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnArchivos;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.RichTextBox txtMensaje;
+        private System.Windows.Forms.TextBox txtAsunto;
+        private System.Windows.Forms.TextBox txtEmailPara;
+    }
+}
+
---
+++ EnvioCorreo/EnvioCorreo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnvioCorreo
+{
+    public partial class fmrEnvioCorreo : Form
+    {
+        private string _myEmail = ""; // Tu dirección de correo electrónico
+        private string _Contraseña = ""; // Tu contraseña (¡asegúrate de mantenerla segura!)
+        private string Alias = ""; // Tu alias o nombre
+        private string[] _ArchivosAdjuntos; // Arreglo para almacenar los archivos adjuntos
+        private MailMessage _mCorreo; // Mensaje de correo electrónico
+
+        public fmrEnvioCorreo()
+        {
+            InitializeComponent();
+        }
+
+        private void CrearCuerpoCorreo()
+        {
+            _mCorreo = new MailMessage();
+            _mCorreo.From = new MailAddress(_myEmail, Alias, System.Text.Encoding.UTF8); // Establece la dirección y el alias del remitente
+            _mCorreo.To.Add(txtEmailPara.Text.Trim()); // Agrega el destinatario desde el cuadro de texto
+            _mCorreo.Subject = txtAsunto.Text.Trim(); // Asigna el asunto desde el cuadro de texto
+            _mCorreo.Body = txtMensaje.Text.Trim(); // Asigna el cuerpo del mensaje desde el cuadro de texto
+            _mCorreo.IsBodyHtml = true; // Indica que el cuerpo es HTML
+            _mCorreo.Priority = MailPriority.High; // Establece la prioridad del correo como alta
+
+            // Adjunta los archivos seleccionados
+            for (int i = 0; i < _ArchivosAdjuntos.Length; i++)
+            {
+                _mCorreo.Attachments.Add(new Attachment(_ArchivosAdjuntos[i]));
+            }
+        }
+
+        private void Enviar()
+        {
+            try
+            {
+                SmtpClient smtp = new SmtpClient();
+                smtp.UseDefaultCredentials = false;
+                smtp.Port = 25; // Puerto SMTP
+                smtp.Host = "smtp.gmail.com"; // Servidor SMTP de Gmail
+                smtp.Credentials = new System.Net.NetworkCredential(_myEmail, _Contraseña); // Credenciales de inicio de sesión
+                // La línea a continuación evita la validación del certificado SSL (¡no recomendado en producción!)
+                // ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors) { return true; };
+                smtp.EnableSsl = true; // Habilita SSL para conexiones seguras
+                smtp.Send(_mCorreo); // Envía el correo
+                MessageBox.Show("Enviado"); // Muestra un mensaje de confirmación
+                lblArchivos.Text = ""; // Limpia la etiqueta de archivos adjuntos
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message); // Muestra un mensaje de error si ocurre una excepción
+            }
+        }
+
+        private void AdjuntarArchivos()
+        {
+            var names = "";
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true; // Permite seleccionar múltiples archivos
+            ofd.Title = "Adjuntar archivos al correo"; // Título del cuadro de diálogo
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                _ArchivosAdjuntos = ofd.FileNames; // Almacena los nombres de los archivos seleccionados
+            }
+
+            for (int i = 0; i < _ArchivosAdjuntos.Length; i++)
+            {
+                names += _ArchivosAdjuntos[i] + "\n"; // Muestra los nombres de los archivos seleccionados
+            }
+
+            lblArchivos.Text = names; // Muestra los nombres de los archivos en una etiqueta
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            CrearCuerpoCorreo();
+            Enviar();
+        }
+
+        private void btnFiles_Click(object sender, EventArgs e)
+        {
+            AdjuntarArchivos();
+        }
+    }
+}
